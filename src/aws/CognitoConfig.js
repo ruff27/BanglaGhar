6205d4748f53678e8ac3309ac787ad9faaabdd45
prabesh51 <105505930+prabesh51@users.noreply.github.com>
2025-03-31 08:47:08 +1100

    Update CognitoConfig.js

--- src/aws/CognitoConfig.js
+++ src/aws/CognitoConfig.js
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 
 const poolData = {
-  UserPoolId: "ap-southeast-2_vtwd9UV4E", // Replace with your actual User Pool ID
-  ClientId: "745l6e2msldreakerhoor5f1no",   // Replace with your SPA Client ID (no secret)
+  UserPoolId: process.env.USERPOOLID, // Replace with your actual User Pool ID
+  ClientId: process.env.CLIENTID,   // Replace with your SPA Client ID (no secret)
 };
 
 const userPool = new CognitoUserPool(poolData);
